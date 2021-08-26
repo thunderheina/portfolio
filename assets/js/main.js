90a84654a27678b77b50db3f283504060a8584d1
thunderheina <rheinadlon@gmail.com>
2021-08-26 14:32:18 +0700

    add services modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,27 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
 	el.addEventListener('click', toggleSkills)
 })
+
+/* === SERVICES MODAL === */
+const modalViews = document.querySelectorAll('.services__modal'),
+	modalBtns = document.querySelectorAll('.services__button'),
+	modalClose = document.querySelectorAll('.services__modal-close');
+
+let modal = function (modalClick) {
+	modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+	modalBtn.addEventListener('click', () => {
+		modal(i)
+	})
+})
+
+modalClose.forEach((modalClose) => {
+	modalClose.addEventListener('click', () => {
+		modalViews.forEach((modalView) => {
+			modalView.classList.remove('active-modal')
+		})
+	})
+})
+
